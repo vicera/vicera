28260d18aee48e9a2aa4a7b0f8a3ccae5347dc9c
h34ting4ppliance <h34ting4ppliance@protonmail.com>
2020-09-06 16:31:45 +0200

    Implemented controller emulation.

--- main.c
+++ main.c
@@ -15,14 +15,17 @@
 
 #include "cpu.h"
 #include "gpu.h"
+#include "controller.h"
 #include "logging.h"
 
 #define TSCREEN_X 160
 #define TSCREEN_Y 160
 
 #define FNAME "main.c"
+
 struct CPU console;
 struct GPU console_gpu;
+struct Controller console_ctrl;
 
 bool done;
 
@@ -32,7 +35,9 @@ void* gpu_rendering(void *none)
     // TODO Support scrolling.
     // TODO
     BYTE x, y;
-    BYTE sx, sy;
+    BYTE sx, sy, controller;
+    unsigned int k;
+    controller = 0;
 
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
@@ -48,6 +53,13 @@ void* gpu_rendering(void *none)
     // Create window
     SDL_CreateWindowAndRenderer(TSCREEN_X*2, TSCREEN_Y*2, 0,
                                 &win, &renderer);
+    if ((!win) || (!renderer))
+    {
+        logging_error(FNAME, "[SDL] Window creation failed.");
+        perror("SDL Error");
+
+        exit(1);
+    }   
 
     while (!done)
     {
@@ -71,13 +83,39 @@ void* gpu_rendering(void *none)
                     SDL_RenderDrawPoint(renderer, x*2, y*2+1);
                     SDL_RenderDrawPoint(renderer, x*2+1, y*2+1);
                 }
-
-        if (SDL_PollEvent(&event))
+        
+        while (SDL_PollEvent(&event))
+        {
             if (event.type == SDL_QUIT)
             {
                 done = true;
                 console.running = false;
+            } else if (event.type == SDL_KEYDOWN)
+            {
+                k = event.key.keysym.sym;
+                if (k == console_ctrl.up) controller |= 0x80;
+                if (k == console_ctrl.right) controller |= 0x40;
+                if (k == console_ctrl.down) controller |= 0x20;
+                if (k == console_ctrl.left) controller |= 0x10;
+                if (k == console_ctrl.start) controller |= 0x08;
+                if (k == console_ctrl.select) controller |= 0x04;
+                if (k == console_ctrl.a) controller |= 0x02;
+                if (k == console_ctrl.b) controller |= 0x01;
+            } else if (event.type == SDL_KEYUP)
+            {
+                k = event.key.keysym.sym;
+                if (k == console_ctrl.up) controller &= ~0x80;
+                if (k == console_ctrl.right) controller &= ~0x40;
+                if (k == console_ctrl.down) controller &= ~0x20;
+                if (k == console_ctrl.left) controller &= ~0x10;
+                if (k == console_ctrl.start) controller &= ~0x08;
+                if (k == console_ctrl.select) controller &= ~0x04;
+                if (k == console_ctrl.a) controller &= ~0x02;
+                if (k == console_ctrl.b) controller &= ~0x01;
+                
             }
+        }
+        console.memory[0xfff4] = controller;
 
         SDL_RenderPresent(renderer);
         SDL_Delay(1000 / 30);
@@ -107,6 +145,17 @@ int main(int argc, char **argv)
     if (argc != 2)
         print_usage();
     
+    console_ctrl.up     = SDLK_UP;
+    console_ctrl.right  = SDLK_RIGHT;
+    console_ctrl.left   = SDLK_LEFT;
+    console_ctrl.down   = SDLK_DOWN;
+
+    console_ctrl.start  = SDLK_RETURN;
+    console_ctrl.select = SDLK_BACKSPACE;
+    
+    console_ctrl.a      = SDLK_z;
+    console_ctrl.b      = SDLK_x;
+
     FILE *rom = fopen(argv[1], "r");
     if (!rom)
     {
